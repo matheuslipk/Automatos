b3f78bf302430ea1df02a16ea5551f453280faf3
AUTOLOGON <AUTOLOGON@192.168.0.195>
2017-05-18 12:08:06 -0300

    Melhorias

--- src/altomatos/Identificador.java
+++ src/altomatos/Identificador.java
@@ -2,6 +2,7 @@ package altomatos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Identificador {
    
@@ -9,69 +10,65 @@ public class Identificador {
       List<String> lista = new ArrayList<>();  
       int estadoAtual = 0;
       String temp = "";
+      Scanner texto = new Scanner(str);
       
-      for(int i=0; i<str.length(); i++){
-         char caracter = (str.charAt(i));
-         
-         switch(estadoAtual){
-            case(0):{
-               if(Ultilit.isLetra(caracter)){
-                  estadoAtual = 1;
-                  temp+=caracter;
-                  break;
-               }else{
+      while(texto.hasNext()){
+         String str2 = texto.next();
+         sairfor: for(int i=0; i<str2.length(); i++){
+            char caracter = (str2.charAt(i));
+
+            switch(estadoAtual){
+               case(0):{
+                  if(Ultilit.isLetra(caracter)){
+                     estadoAtual = 1;
+                     temp+=caracter;
+                     break;
+                  }else{
+                     temp="";
+                     estadoAtual = 0;
+                     break sairfor;
+                  }
+               }
+               case(1):{
+                  if(Ultilit.isLetra(caracter) || Ultilit.isNumeric(caracter)){
+                     estadoAtual = 2;
+                     temp+=caracter;
+                     break;
+                  }
+
+                  if(caracter=='_'){
+                     estadoAtual = 3;
+                     temp+=caracter;
+                     break;
+                  }
                   temp="";
-                  estadoAtual = 0;
-                  break;
+                  break sairfor;
+               }
+               case(2):{
+                  if(Ultilit.isLetra(caracter) || Ultilit.isNumeric(caracter)){
+                     estadoAtual = 2;
+                     temp+=caracter;
+                     break;
+                  }
+                  temp="";
+                  break sairfor;
+               }
+               case(3):{
+                  if(Ultilit.isLetra(caracter) || Ultilit.isNumeric(caracter)){
+                     estadoAtual = 2;
+                     temp+=caracter;
+                     break;
+                  }
+                  temp="";
+                  break sairfor;
                }
             }
-            case(1):{
-               if(Ultilit.isLetra(caracter) || Ultilit.isNumeric(caracter)){
-                  estadoAtual = 2;
-                  temp+=caracter;
-                  break;
-               }
-               
-               if(caracter=='_'){
-                  estadoAtual = 3;
-                  temp+=caracter;
-                  break;
-               }else{
-                  lista.add(temp);
-                  temp="";
-                  estadoAtual = 0;
-                  break;
-               }
-            }
-            case(2):{
-               if(Ultilit.isLetra(caracter) || Ultilit.isNumeric(caracter)){
-                  estadoAtual = 2;
-                  temp+=caracter;
-                  break;
-               }else{
-                  lista.add(temp);
-                  temp="";
-                  estadoAtual = 0;
-                  break;
-               }
-            }
-            case(3):{
-               if(Ultilit.isLetra(caracter) || Ultilit.isNumeric(caracter)){
-                  estadoAtual = 2;
-                  temp+=caracter;
-                  break;
-               }else{
-                  lista.add(temp);
-                  temp="";
-                  estadoAtual = 0;
-                  break;
-               }
-            }
+            
          }
          
+         if (temp.length()>0) lista.add(temp);
+         temp="";
       }
-      if (temp.length()>0) lista.add(temp);
-      
       return lista;
    }
    
--- src/altomatos/Main.java
+++ src/altomatos/Main.java
@@ -3,9 +3,12 @@ import java.util.List;
 
 public class Main {
    public static void main(String[] args) {
-      List<String> l = Identificador.listarIdentificadores("mathue_a__lo123456 asa");
+      List<String> l = Identificador.listarIdentificadores("ab1ab a_a asa1");
       for(int i=0; i<l.size(); i++){
          System.out.println(l.get(i));
       }
+      
+      
+      
    }
 }
